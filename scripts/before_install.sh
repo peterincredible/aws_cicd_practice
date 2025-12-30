e4a88f343d2d37d4034a93322d38d72f1d93fd70
peter dibie <ekene.dibie@gmail.com>
2025-12-30 21:30:50 +0100

    made some changes today 1

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#download node and npm
+# Download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
 . ~/.nvm/nvm.sh
+
 nvm install node
 
-#create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
+# Create our working directory if it doesnt exist
+DIR="/home/ec2-user/nodejs-app"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
